219efecb96a3bbc6464bcb35091f8bc26e8800a0
gopal166 <109658563+gopal166@users.noreply.github.com>
2022-07-20 20:39:34 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,39 @@
+rom django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+# Create your models here.
+
+class Destinations(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+  
+class Profile(modes.Model):
+    gender_choice = (
+        ('Male', 'Male')
+        ('Female', 'Female')
+        ('Others', 'Others')
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    Name = models.CharField(max_length=50)
+    DOB = models.DateField()
+    gender = models.CharField(max_length=50, choices = gender_choice, default='Male')
+    phone = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.Name
+    
+class UserAddress(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    address1 = models.CharField(max_length=50)
+    address2 = models.CharField(max_length=50)
+    address3 = models.CharField(max_length=50)
+    pincode = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.owner
